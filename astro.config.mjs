d096e22a190c09b6b80c0f5061b1747cb6713279
sektor30 <sektor9130@gmail.com>
2026-03-23 22:07:45 -0500

    ajuste index.astro con ruta

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 // Modo 'static' para generación estática de todas las páginas
 export default defineConfig({
+   site: 'https://satelite30.github.io',
+  base: '/blog-static-popular-esmedellinDev-',
   output: 'static'
 });
